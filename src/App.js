3fbad66b44592bdce76eee331775080a1c8cfd91
Carolina Aguilar-Restrepo <aguilar.carolina10@gmail.com>
2022-08-24 17:03:18 -0400

    update

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ const App = () => {
   return (
     <div className="App">
       <NavBar />
-      <h1>Real Legos Set List</h1>
-      {legos.map((legos) => (
-        <Legos
-          lego_set_name={legos.lego_set_name}
-          lego_set_image={legos.lego_set_image}
-          real_image={legos.real_image}
-          price={legos.price}
-        />
-      ))}
+      <h1>Legos Sets Inspired By Real Things</h1>
+      <div className="card">
+        {legos.map((legos) => (
+          <Legos
+            lego_set_name={legos.lego_set_name}
+            lego_set_image={legos.lego_set_image}
+            real_image={legos.real_image}
+            price={legos.price}
+          />
+        ))}
+      </div>
     </div>
   )
 }
