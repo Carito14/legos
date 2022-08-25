e899585ee4d9aa603a8e129b3a8696a1122dfec8
Carolina Aguilar-Restrepo <aguilar.carolina10@gmail.com>
2022-08-24 20:42:54 -0400

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ const NavBar = () => {
     <nav className="nav-bar">
       <NavItems content="Home" />
       <NavItems content="Lego Store" />
+      <NavItems content="Carolina's Wish List" />
       <NavItems content="More Info" />
     </nav>
   )
